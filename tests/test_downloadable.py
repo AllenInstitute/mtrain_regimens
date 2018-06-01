4a80d01910a517a0e1bc66c99878605e4637e31a
nicholasc <nicain.seattle@gmail.com>
2018-06-01 09:56:01 -0700

    throw warning on commit if name is already registered

--- tests/test_downloadable.py
+++ tests/test_downloadable.py
@@ -4,8 +4,9 @@ import hashlib
 import warnings
 import requests
 
+regimen_data = yaml.load(open(os.path.join(os.path.dirname(__file__), '..', 'regimen.yml'), 'r'))
+
 def test_script_files_download():
-    regimen_data = yaml.load(open(os.path.join(os.path.dirname(__file__), '..', 'regimen.yml'), 'r'))
 
     for stage_name, stage_dict in regimen_data['stages'].items(): 
         if stage_dict['script'] == 'NotImplemented':
@@ -21,3 +22,13 @@ def test_script_files_download():
                 
             else:
                 assert hashlib.md5(result.content).hexdigest() == stage_dict['script_md5']
+
+def test_add_already_tracked_regimen():
+
+    api_endpoint = 'http://prodmtrain1:5000/api/regimens'
+    result = requests.get(api_endpoint)
+    assert result.status_code == 200
+    data = result.json()
+    assert data['total_pages'] == 1
+    if regimen_data['name'] in [x['name'] for x in data['objects']]:
+        warnings.warn('Regimen name %s already tracked on server %s' % (regimen_data['name'], api_endpoint))
