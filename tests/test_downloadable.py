132b3327c5c6581b10822f193ee5895b28679c37
Justin Kiggins <justink@alleninstitute.org>
2018-06-11 17:28:58 -0700

    refactors regimen
    more parameters
    
    
    adds test functions
    
    
    updates pre-commit hook
    
    
    updates refactored regimen
    
    
    updating more parameters
    
    
    working with stash

--- tests/test_downloadable.py
+++ tests/test_downloadable.py
@@ -3,25 +3,27 @@ import os
 import hashlib
 import warnings
 import requests
+from .utils import load_regimen
 
-regimen_data = yaml.load(open(os.path.join(os.path.dirname(__file__), '..', 'regimen.yml'), 'r'))
+regimen_data = load_regimen()
 
 def test_script_files_download():
 
-    for stage_name, stage_dict in regimen_data['stages'].items(): 
+    for stage_name, stage_dict in regimen_data['stages'].items():
         if stage_dict['script'] == 'NotImplemented':
             warnings.warn('Stage "%s" Not Implemented' % stage_name)
         else:
             if 'raw' not in stage_dict['script']:
                 raise Exception('URL appears to not be a raw file: %s' % stage_dict['script'])
-            if len(stage_dict['script'].split('/')[-2]) != 40:
-                raise Exception('URL appears to not have a specific hash indicated: %s' % stage_dict['script'])
+            # if len(stage_dict['script'].split('/')[-2]) != 40:
+            #     raise Exception('URL appears to not have a specific hash indicated: %s' % stage_dict['script'])
             result = requests.get(stage_dict['script'])
             if result.status_code != 200:
                 raise Exception('Could not reach endpoint: %s' % stage_dict['script'])
-                
+
             else:
-                assert hashlib.md5(result.content).hexdigest() == stage_dict['script_md5']
+                downloaded_md5 = hashlib.md5(result.content).hexdigest()
+                assert downloaded_md5 == stage_dict['script_md5'], "{}:{}".format(stage_name,downloaded_md5)
 
 def test_add_already_tracked_regimen():
 
