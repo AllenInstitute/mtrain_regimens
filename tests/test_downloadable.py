410f382e0b34e6d8f0e7df835bff10e03aa9cfee
nicholasc <nicain.seattle@gmail.com>
2019-03-11 19:01:00 -0700

    Added .bamboo folder
    updated test_downloadable to properly check if regimen already tracked

--- tests/test_downloadable.py
+++ tests/test_downloadable.py
@@ -3,13 +3,47 @@ import os
 import hashlib
 import warnings
 import requests
-from .utils import load_regimen
+import sys
+import itertools
+import pandas as pd
 
-regimen_data = load_regimen()
+'''
+Copy-pasted get_page and get_df; This is code duplication, but I dont want this to be an installed package, and both tests and scripts need this code
+'''
 
-def test_script_files_download():
+def get_page(table_name, api_base='http://prodmtrain1:5000', get_obj=None, **kwargs):
 
-    for stage_name, stage_dict in regimen_data['stages'].items():
+    if get_obj is None:
+        get_obj = requests
+
+    data = {'total_pages':'--'}
+    for ii in itertools.count(1):
+        print('Downloading page: %s/%s ' % (ii , data['total_pages']), end='')
+        sys.stdout.flush()
+
+        tmp = get_obj.get(os.path.join(api_base, "api/v1/%s?page=%i" % (table_name, ii)), **kwargs)
+        try:
+            data = tmp.json()
+        except TypeError:
+            data = tmp.json
+        print('... ', end='')
+        if 'message' not in data:
+            df = pd.DataFrame(data["objects"])
+            print('done')
+            sys.stdout.flush()
+            yield df
+
+        if 'total_pages' not in data or data['total_pages'] == ii:
+            print('done')
+            return
+
+def get_df(table_name, api_base='http://prodmtrain1:5000', get_obj=None, **kwargs):
+    return pd.concat([df for df in get_page(table_name, api_base=api_base, get_obj=get_obj, **kwargs)], axis=0)
+
+
+def test_script_files_download(regimen_dict):
+
+    for stage_name, stage_dict in regimen_dict['stages'].items():
         if stage_dict['script'] == 'NotImplemented':
             warnings.warn('Stage "%s" Not Implemented' % stage_name)
         else:
@@ -25,12 +59,9 @@ def test_script_files_download():
                 downloaded_md5 = hashlib.md5(result.content).hexdigest()
                 assert downloaded_md5 == stage_dict['script_md5'], "{}:{}".format(stage_name,downloaded_md5)
 
-def test_add_already_tracked_regimen():
+def test_add_already_tracked_regimen(regimen_dict):
 
-    api_endpoint = 'http://prodmtrain1:5000/api/v1/regimens'
-    result = requests.get(api_endpoint)
-    assert result.status_code == 200
-    data = result.json()
-    assert data['total_pages'] >= 1
-    if regimen_data['name'] in [x['name'] for x in data['objects']]:
-        warnings.warn('Regimen name %s already tracked on server %s' % (regimen_data['name'], api_endpoint))
+    api_base = 'http://prodmtrain1:5000'
+    regimen_list = get_df('regimens', api_base=api_base)['name'].values
+    if regimen_dict['name'] in regimen_list:
+        raise Exception('Regimen name %s already tracked on server %s' % (regimen_dict['name'], api_base))
